695c7dccc7e9d8b5fd373fce7d8e0bcefec07e16
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2022-04-06 00:15:58 -0500

    se agrego la normalizacion de forma global, se termino el login de estudiante en escritorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/loginEstudent/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
