5907ece87f2b44ddd19ce3fc254073bef587dbc8
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2022-04-06 23:50:11 -0500

    agrega el apartado de preguntas, falta los estilos de los input y botones y corregir la seleccion de inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/loginEstudent/login.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
+import { QuestionComponent } from './pages/question/question.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+
     NavbarComponent,
-    LoginAdminComponent
+     QuestionComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maestros.ts
@@ -0,0 +1,22 @@
+export const maestros  = [
+    {
+        nombre: "Roberto Rangel",
+        materia: "Topicos Avanzados ",
+        academia: "ISC"
+    },
+    {
+        nombre: "Veronica Sobrevilla",
+        materia: "Ingenieria de software",
+        academia: "ISC"
+    },
+    {
+        nombre: "Rosa Pelayo",
+        materia: "Redes de Computadora",
+        academia: "ISC"
+    },
+    {
+        nombre: "Paulino Ramirez",
+        materia: "Interfaz Graficas",
+        academia: "ISC"
+    }
+]
