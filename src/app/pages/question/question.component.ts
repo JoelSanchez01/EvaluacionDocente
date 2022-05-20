fbb1678d0437c5663e92b7d7b16209a264a8acbf
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-05-20 00:50:34 -0500

    agega la forma de desplazarnos entre preguntas, mostrando en consola el nombre de maestro, la califcacion asiganda en pregunta y la categoria para despues ir a la siguiente pregunta y limpiar el formulario

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { maestros } from "../../maestros"
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { preguntas } from "../../preguntas"
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
@@ -17,19 +16,25 @@ export class QuestionComponent implements OnInit {
   araregloNumeros: Number[] = [5, 4, 3, 2, 1]
   nombreEstudiante: String = "Tilin Martinez Hernandez"
   maestrosclase = maestros;
+
   numero: String = "";
   validator: boolean = true;
+  preguntaIDL: number = 0;
 
+  preguntaPagina = preguntas[this.preguntaIDL];
 
-  validacion(event: any) {
-
-
-  }
 
 
   getValues(val: any) {
     if (val.status == "INVALID") alert("Responde todas las preguntas")
-    else console.log(val.value);
+    else {
+      // console.log(val.value)
+      console.log(val.value);
+      console.log(this.preguntaPagina.clasificacion);
+      this.preguntaIDL++
+      this.preguntaPagina = preguntas[this.preguntaIDL];
+      val.reset();
+    };
   }
 
 
