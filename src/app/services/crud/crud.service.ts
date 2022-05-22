c3d82ad99d5e637087e0f3514db390932d5fbd48
Antonio Reyna Espinoza <antoniore102001@gmail.com>
2022-05-22 12:53:23 -0500

    Agrega servicio para CRUD

---
+++ src/app/services/crud/crud.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor() { }
+}
