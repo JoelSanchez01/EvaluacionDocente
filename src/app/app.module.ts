f8fe13ba7c4e564e0aa891b063a227a1e68b1c0b
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-06-14 21:53:17 -0500

    Agrega la tabla con paginator funcional, el select hace la consulta al momento de cambiar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
 import { UploadDataComponent } from './pages/upload-data/upload-data.component';
 import { ControlPeriodoComponent } from './components/control-periodo/control-periodo.component';
 import { ResultadosComponent } from './components/resultados/resultados.component';
-
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
@@ -18,7 +17,12 @@ import { QuestionComponent } from './pages/question/question.component';
 import { MenuAdminComponent } from './pages/menu-admin/menu-admin.component';
 import { EditDatabaseComponent } from './pages/edit-database/edit-database.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -43,8 +47,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/edit-database/edit-database.component.ts
+++ src/app/pages/edit-database/edit-database.component.ts
@@ -1,15 +1,175 @@
-import { Component, OnInit } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+import { AfterViewInit, Component, KeyValueDiffers, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CrudService } from 'src/app/services/crud/crud.service';
+import { Alumno } from "../../models/Alumno"
+
+
+
+
+
+
+
+
 
 @Component({
   selector: 'app-edit-database',
   templateUrl: './edit-database.component.html',
   styleUrls: ['./edit-database.component.scss']
+
 })
-export class EditDatabaseComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+export class EditDatabaseComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource!: MatTableDataSource<Alumno>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+
+
+  cargando: boolean = true;
+
+  constructor(public crud: CrudService) {
+    // Create 100 users
+    // Assign the data to the data source for the table to render
   }
 
+  private iterador = 1;
+
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  Valores: any[] = []
+
+  onChange(valor: any) {
+    console.log(valor.value)
+    if (valor.value == "Alumnos") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getStudent().subscribe(data => {
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+    }
+    if (valor.value == "Personal") {
+      this.crud.getTeachers().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+    }
+    if (valor.value == "Academias") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getAcademy().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+
+    }
+
+    if (valor.value == "Grupos") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getGroup().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+
+    }
+
+    if (valor.value == "Periodos") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getPeriod().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+
+    }
+
+    if (valor.value == "Materias") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getSubject().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+
+    }
+
+    if (valor.value == "Carreras") {
+      this.Valores.splice(0, this.Valores.length)
+      this.crud.getCareer().subscribe(data => {
+        this.Valores.splice(0, this.Valores.length)
+        this.Valores = data;
+        this.dataSource = new MatTableDataSource(this.Valores);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.cargando = false;
+        console.log(data)
+
+      })
+
+    }
+
+
+
+
+  }
+
+
 }
+
+
