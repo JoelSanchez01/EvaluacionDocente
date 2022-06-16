b47d55f8ea246238c060e259e7d6d9e8641f79ec
Antonio Reyna Espinoza <36315976+fwcheesecake@users.noreply.github.com>
2022-06-16 08:25:38 -0500

    Revert "F implementing update component j"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login-student/login.component';
@@ -9,66 +9,42 @@ import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
 import { UploadDataComponent } from './pages/upload-data/upload-data.component';
 import { ControlPeriodoComponent } from './components/control-periodo/control-periodo.component';
 import { ResultadosComponent } from './components/resultados/resultados.component';
+
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AdminComponent } from './pages/admin/admin.component';
 import { QuestionComponent } from './pages/question/question.component';
 import { MenuAdminComponent } from './pages/menu-admin/menu-admin.component';
 import { EditDatabaseComponent } from './pages/edit-database/edit-database.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CommonModule } from '@angular/common';
-import { TableStudentsComponent } from './components/table-students/table-students.component';
-import { TableTeacherComponent } from './components/table-teacher/table-teacher.component';
-import { TableAcademyComponent } from './components/table-academy/table-academy.component';
-import { TablePeriodComponent } from './components/table-period/table-period.component';
-import { TableSubjectComponent } from './components/table-subject/table-subject.component';
-import { TableGroupComponent } from './components/table-group/table-group.component';
-import { TableCareerComponent } from './components/table-career/table-career.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
+
     NavbarComponent,
     ControlPeriodoComponent,
     ResultadosComponent,
     UploadDataComponent,
+
     LoginComponent,
     LoginAdminComponent,
     QuestionComponent,
     AdminComponent,
     MenuAdminComponent,
     EditDatabaseComponent,
-    UploadDataComponent,
-    TableStudentsComponent,
-    TableTeacherComponent,
-    TableAcademyComponent,
-    TablePeriodComponent,
-    TableSubjectComponent,
-    TableGroupComponent,
-    TableCareerComponent
+    UploadDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatInputModule,
-    NgbModule,
     FormsModule,
     ReactiveFormsModule,
-    CommonModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/edit-database/edit-database.component.ts
+++ src/app/pages/edit-database/edit-database.component.ts
@@ -1,81 +1,15 @@
-import { Component } from '@angular/core';
-
-
-
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit-database',
   templateUrl: './edit-database.component.html',
   styleUrls: ['./edit-database.component.scss']
-
 })
+export class EditDatabaseComponent implements OnInit {
 
+  constructor() { }
 
-
-export class EditDatabaseComponent {
-
-
-  constructor() {
+  ngOnInit(): void {
   }
-
-
-
-
-
-
-
-  Seleccionador = "Alumnos";
-
-
-
-  onChange(valor: any) {
-    if (valor.value == "Alumnos") {
-      this.Seleccionador = "Alumnos";
-    }
-    if (valor.value == "Personal") {
-      this.Seleccionador = "Personal";
-    }
-    if (valor.value == "Academias") {
-      this.Seleccionador = "Academias";
-    }
-
-    if (valor.value == "Grupos") {
-      this.Seleccionador = "Grupos";
-    }
-
-    if (valor.value == "Periodos") {
-      this.Seleccionador = "Periodos";
-    }
-
-    if (valor.value == "Materias") {
-      this.Seleccionador = "Materias";
-    }
-
-    if (valor.value == "Carreras") {
-      this.Seleccionador = "Carreras";
-    }
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
-
-
