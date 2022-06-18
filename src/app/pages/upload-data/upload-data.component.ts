7b13df33635a2dd2eac22c0cbd173697653e9b08
Antonio Reyna Espinoza <antoniore102001@gmail.com>
2022-06-18 00:59:12 -0500

    Mas soporte para agregar correos de alumnos

--- src/app/pages/upload-data/upload-data.component.ts
+++ src/app/pages/upload-data/upload-data.component.ts
@@ -1,28 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import * as XLSX from "xlsx";
+import * as XLSX from 'xlsx';
 
-import { Alumno } from "../../models/Alumno";
-import { Materia } from "../../models/Materia";
-import { Periodo } from "../../models/Periodo";
-import { Academia } from "../../models/Academia";
+import { Alumno } from '../../models/Alumno';
+import { Materia } from '../../models/Materia';
+import { Periodo } from '../../models/Periodo';
+import { Academia } from '../../models/Academia';
 import { Carrera } from 'src/app/models/Carrera';
-import { Docente } from "../../models/Docente";
-import { SeleccionMateria } from "../../models/SeleccionMateria";
-import { Grupo } from "../../models/Grupo";
-import { CrudService } from "../../services/crud/crud.service";
-import { AuthService } from "../../services/auth/auth.service";
+import { Docente } from '../../models/Docente';
+import { SeleccionMateria } from '../../models/SeleccionMateria';
+import { Grupo } from '../../models/Grupo';
+import { CrudService } from '../../services/crud/crud.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-upload-data',
   templateUrl: './upload-data.component.html',
-  styleUrls: ['./upload-data.scss']
+  styleUrls: ['./upload-data.scss'],
 })
 export class UploadDataComponent implements OnInit {
+  constructor(public crud: CrudService, public auth: AuthService) {}
 
-  constructor(public crud: CrudService, public auth: AuthService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   academias: Academia[] = [];
   materias: Materia[] = [];
@@ -33,24 +31,34 @@ export class UploadDataComponent implements OnInit {
   grupos: Grupo[] = [];
   seleccionMaterias: SeleccionMateria[] = [];
 
-  archivos: String[] = ["Organigrama", "Materias", "Alumnos", "Periodos", "Carreras", "Personal", "Grupos", "Seleccion de Materias"];
+  archivos: String[] = [
+    'Organigrama',
+    'Materias',
+    'Alumnos',
+    'Periodos',
+    'Carreras',
+    'Personal',
+    'Grupos',
+    'Seleccion de Materias',
+  ];
 
   fileUpload(event1: any) {
-
     const selectedFile = event1.target.files[0];
-    const fileReader = new FileReader;
+    const fileReader = new FileReader();
     fileReader.readAsBinaryString(selectedFile);
 
     fileReader.onload = (event: any) => {
       let binaryData = event.target.result;
       let workbook = XLSX.read(binaryData, { type: 'binary' });
 
-      workbook.SheetNames.forEach(sheet => {
-        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet], { header: 1 });
+      workbook.SheetNames.forEach((sheet) => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet], {
+          header: 1,
+        });
         data.shift();
 
-        if (event1.srcElement.__ngContext__[8].$implicit == "Organigrama") {
-          this.academias = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Organigrama') {
+          this.academias = data.map((item) => {
             return new Academia(
               // @ts-ignore
               item[0],
@@ -58,11 +66,11 @@ export class UploadDataComponent implements OnInit {
               item[1],
               // @ts-ignore
               item[2]
-            )
+            );
           });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Materias") {
-          this.materias = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Materias') {
+          this.materias = data.map((item) => {
             return new Materia(
               // @ts-ignore
               item[0],
@@ -75,36 +83,69 @@ export class UploadDataComponent implements OnInit {
               // @ts-ignore
               item[4],
               // @ts-ignore
-              item[5],
-            )
+              item[5]
+            );
           });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Alumnos") {
-          this.alumnos = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Alumnos') {
+          this.alumnos = data.map((item) => {
+            // @ts-ignore
+            if(Object.keys(item).length === 0) {
+              return new Alumno('', '', '', '', '', '', '', '', '');
+            }
+
+            // @ts-ignore
+            let control: string = <string>item[0];
+            // @ts-ignore
+            let clave_carrera: string = <string>item[1];
+            // @ts-ignore
+            let reticula: string = <string>item[2];
+            // @ts-ignore
+            let semestre: string = <string>item[3];
+            // @ts-ignore
+            let estado: string = <string>item[4];
+            // @ts-ignore
+            let plan_estudios: string = <string>item[5];
+
+            // @ts-ignore
+            let nombre = <string> typeof item[8] === 'number' ? item[8].toString() : item[8],
+            // @ts-ignore
+              paterno: string = <string> typeof item[6] === 'number' ? item[6].toString() : item[6],
+            // @ts-ignore
+              materno: string = <string> typeof item[7] === 'number' ? item[7].toString() : item[7];
+
+            let nombre_completo: string = <string> paterno + ' ' + materno + ' ' + nombre;
+
+            // @ts-ignore
+            let nip: string = <string>item[0];
+
+            let correo: string =
+              paterno.replace(/\s/g, '').toLowerCase() +
+              '.' +
+              materno.replace(/\s/g, '').toLowerCase() +
+              '.' +
+              // @ts-ignore
+              item[0].substring(0, 2) +
+              // @ts-ignore
+              item[0].substring(5, 8) +
+              '@itsmante.edu.mx';
+
+
             return new Alumno(
-              // @ts-ignore
-              item[0],
-              // @ts-ignore
-              item[1],
-              // @ts-ignore
-              item[2],
-              // @ts-ignore
-              item[3],
-              // @ts-ignore
-              item[4],
-              // @ts-ignore
-              item[5],
-              // @ts-ignore
-              item[6] + " " + item[7] + " " + item[8],
-              // @ts-ignore
-              item[9],
-              // @ts-ignore
-              item[6].replace(/\s/g, '').toLowerCase() + " " + item[7].replace(/\s/g, '').toLowerCase() + item[0].substring(0, 2) + item[0].substring(5, 8) + "@itsmante.edu.mx"
-            )
+              control,
+              clave_carrera,
+              reticula,
+              semestre,
+              estado,
+              plan_estudios,
+              nombre_completo,
+              nip,
+              correo
+            );
           });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Periodos") {
-          this.periodos = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Periodos') {
+          this.periodos = data.map((item) => {
             return new Periodo(
               // @ts-ignore
               item[0],
@@ -114,11 +155,11 @@ export class UploadDataComponent implements OnInit {
               item[2],
               // @ts-ignore
               item[3]
-            )
+            );
           });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Carreras") {
-          this.carreras = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Carreras') {
+          this.carreras = data.map((item) => {
             return new Carrera(
               // @ts-ignore
               item[0],
@@ -130,11 +171,11 @@ export class UploadDataComponent implements OnInit {
               item[3],
               // @ts-ignore
               item[4]
-            )
-          })
+            );
+          });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Personal") {
-          this.docentes = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Personal') {
+          this.docentes = data.map((item) => {
             return new Docente(
               // @ts-ignore
               item[0],
@@ -145,19 +186,19 @@ export class UploadDataComponent implements OnInit {
               // @ts-ignore
               item[3],
               // @ts-ignore
-              item[9] + " " + item[10] + " " + item[5],
+              item[9] + ' ' + item[10] + ' ' + item[5],
               // @ts-ignore
               item[6],
               // @ts-ignore
               item[8],
               // @ts-ignore
               item[11]
-            )
-          })
+            );
+          });
         }
 
-        if (event1.srcElement.__ngContext__[8].$implicit == "Grupos") {
-          this.grupos = data.map(item => {
+        if (event1.srcElement.__ngContext__[8].$implicit == 'Grupos') {
+          this.grupos = data.map((item) => {
             return new Grupo(
               // @ts-ignore
               item[0],
@@ -171,11 +212,14 @@ export class UploadDataComponent implements OnInit {
               item[4],
               // @ts-ignore
               item[8]
-            )
-          })
+            );
+          });
         }
-        if (event1.srcElement.__ngContext__[8].$implicit == "Seleccion de Materias") {
-          this.seleccionMaterias = data.map(item => {
+        if (
+          event1.srcElement.__ngContext__[8].$implicit ==
+          'Seleccion de Materias'
+        ) {
+          this.seleccionMaterias = data.map((item) => {
             return new SeleccionMateria(
               // @ts-ignore
               item[0],
@@ -186,16 +230,16 @@ export class UploadDataComponent implements OnInit {
               // @ts-ignore
               item[3],
               // @ts-ignore
-              item[4],
-            )
-          })
+              item[4]
+            );
+          });
         }
-      })
-    }
+      });
+    };
   }
 
   SubirBDD(val: any) {
-    if (val.status == "INVALID") alert("Responde todas las preguntas")
+    if (val.status == 'INVALID') alert('Responde todas las preguntas');
     else {
       this.subirOrganigrama(this.academias);
       this.subirPeriodos(this.periodos);
@@ -225,6 +269,7 @@ export class UploadDataComponent implements OnInit {
   }
 
   subirAlumnos(Alumnos: Alumno[]) {
+    console.log(Alumnos);
     this.crud.addStudent(Alumnos).subscribe();
   }
 
